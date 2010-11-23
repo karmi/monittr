0e5fa5f1c9811062ae1884c8ff85c7fbfd0b346b
Karel Minarik <karmi@karmi.cz>
2010-11-23 18:16:00 +0100

    Refactored into Services module and added Services::Filesystem

--- lib/monitr.rb
+++ lib/monitr.rb
@@ -7,30 +7,43 @@ module Monitr
 
   class Monit
 
-    attr_reader :xml, :system
+    attr_reader :xml, :system, :filesystems, :services
 
     def initialize
-      @xml = Nokogiri::HTML(open( MONIT_URL ))
-      @system = System.new(@xml.xpath('//service[@type=5]').first)
+      @xml    = Nokogiri::HTML(open( MONIT_URL ))
+      @system = Services::System.new(@xml.xpath("//service[@type=5]").first)
+      @filesystems = @xml.xpath("//service[@type=0]").map { |xml| Services::Filesystem.new(xml) }
     end
 
   end
 
 
-  class Service < OpenStruct
+  module Services
+    class Base < OpenStruct
+      TYPES = { 0 => "Filesystem", 1 => "Directory", 2 => "File", 3 => "Daemon", 4 => "Connection", 5 => "System" }
+    end
 
-    TYPES = { 0 => "Filesystem", 1 => "Directory", 2 => "File", 3 => "Daemon", 4 => "Connection", 5 => "System" }
+    class System < Base
+      def initialize(xml)
+        super( { :name   => xml.xpath('name').first.content,
+                 :status => xml.xpath('status').first.content.to_i,
+                 :monitored => xml.xpath('monitor').first.content.to_i,
+                 :load   => xml.xpath('system/load/avg01').first.content.to_f,
+                 :cpu    => xml.xpath('system/cpu/user').first.content.to_f,
+                 :memory => xml.xpath('system/memory/percent').first.content.to_f } )
+      end
+    end
 
-  end
-
-  class System < Service
-
-    def initialize(xml)
-      super( { :name   => xml.xpath('name').first.content,
-               :status => xml.xpath('status').first.content.to_i,
-               :load   => xml.xpath('system/load/avg01').first.content.to_f,
-               :cpu    => xml.xpath('system/cpu/user').first.content.to_f,
-               :memory => xml.xpath('system/memory/percent').first.content.to_f } )
+    class Filesystem < Base
+      def initialize(xml)
+        super( { :name    => xml.xpath('name').first.content,
+                 :status  => xml.xpath('status').first.content,
+                 :monitored => xml.xpath('monitor').first.content.to_i,
+                 :percent => xml.xpath('block/percent').first.content.to_f,
+                 :usage   => xml.xpath('block/usage').first.content,
+                 :total   => xml.xpath('block/total').first.content
+              } )
+      end
     end
 
   end
--- test/monitr_test.rb
+++ test/monitr_test.rb
@@ -12,9 +12,19 @@ module Monitr
 
       should "return system info" do
         assert_not_nil     @monit.system
+        assert_equal 1,    @monit.system.monitored
         assert_equal 'myapplication.cz', @monit.system.name
         assert_equal 5.28, @monit.system.load
         assert_equal 0,    @monit.system.status
+      end
+
+      should "return filesystems info" do
+        assert_not_nil @monit.filesystems
+        assert_equal 4, @monit.filesystems.size
+        filesystem = @monit.filesystems.first
+        assert_not_nil filesystem
+        assert_equal 1, filesystem.monitored
+        assert_equal 22.8, filesystem.percent
       end
 
     end
