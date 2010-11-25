bc7bc224f8d194661a43f7a19292f9d7271bbedb
Karel Minarik <karmi@karmi.cz>
2010-11-25 11:14:54 +0100

    Rewrite of example application, so it uses regular views etc, demonistrating the helper usage

--- examples/application.rb
+++ examples/application.rb
@@ -14,5 +14,45 @@ set :monit_urls,  ['http://localhost:2812/_status?format=xml', 'http://localhost
 # set :stylesheet, '/path/to/my/stylesheet'
 
 get "/" do
-  monit_html
+  erb :index
 end
+
+__END__
+
+@@ layout
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Example Admin Application With Monittr</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <style>
+    body     { color: #222; font-family: sans-serif; margin: 2em; }
+    #example { border: 4px solid #ccc; padding: 1em; }
+  </style>
+</head>
+<body>
+  <%= yield %>
+</body>
+</html>
+
+@@ index
+
+<p>This is your regular admin application created with <em>Sinatra</em>, having lots of features.</p>
+
+<p>Now, you can embed the <em>Monittr</em> info anywhere by calling this helper method:</p>
+
+<pre>monit_html</pre>
+
+<p>and you should see something like this:</p>
+
+<div id="example">
+<%= monit_html %>
+</div><!-- /monittr_example -->
+
+<p>See, easy.</p>
+
+<p>You can customize the template, of course, and set the path to it with:</p>
+
+<pre>
+set :template, '/path/to/my/template.erb'
+</pre>
