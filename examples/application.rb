d87c25db68871f687653577aaf0581e6d3964a2e
Karel Minarik <karmi@karmi.cz>
2010-11-28 08:48:55 +0100

    Adding documentation in example application

--- examples/application.rb
+++ examples/application.rb
@@ -7,16 +7,28 @@ require 'fakeweb'
 
 require 'monittr/sinatra/monittr'
 
-FakeWeb.register_uri(:get, 'http://localhost:2812/_status?format=xml', :body => File.read( File.expand_path('../../test/fixtures/status.xml', __FILE__)))
+# --- Comment these lines to enable loading data from http://localhost:2812 ---
+FakeWeb.register_uri(:get, 'http://localhost:2812/_status?format=xml',
+                     :body => File.read( File.expand_path('../../test/fixtures/status.xml', __FILE__)))
+# -----------------------------------------------------------------------------
 
+# --- Set URLs to your Monit instances here -----------------------------------
 set :monit_urls,  %w[ http://localhost:2812 http://localhost:2812 ]
+# -----------------------------------------------------------------------------
 
+# --- Use your own template ---------------------------------------------------
 # set :template,   Proc.new { File.join(root, 'template.erb') }
+# -----------------------------------------------------------------------------
+
+# --- Use your own stylesheet for the `stylesheet` helper ---------------------
 # set :stylesheet, '/path/to/my/stylesheet'
+# -----------------------------------------------------------------------------
+
 
 get "/" do
   erb :index
 end
+
 
 __END__
 
@@ -28,7 +40,8 @@ __END__
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <style>
     body     { color: #222; font-family: sans-serif; margin: 2em; }
-    #example { border: 4px solid #ccc; padding: 1em; }
+    p        { margin: 0 0 0.5em 0; }
+    #example { border: 4px solid #ccc; padding: 1em; margin: 1em 0 1em 0; }
   </style>
 </head>
 <body>
