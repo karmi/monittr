086c3e93b8a53d60c2fe4632f2a2f497b4552336
Garami Gábor <hron@hron.me>
2014-06-13 10:24:29 +0200

    Fixing failing timeout test
    Moing to FakeWeb's default timeout mock, because it is more reliable
    than mocha's way, and imitates the real situation more closely.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,6 +17,7 @@ class Test::Unit::TestCase
   def setup
     FakeWeb.register_uri(:get, 'http://admin:monit@localhost:2812/_status?format=xml', :body => fixture_file('status.xml'))
     FakeWeb.register_uri(:get, 'http://not-working/_status?format=xml', :body => '', :status => ['500'])
+    FakeWeb.register_uri(:get, 'http://admin@timeout.net:2812/_status?format=xml', :exception => Timeout::Error)
     FakeWeb.allow_net_connect = false
   end
 
